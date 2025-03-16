4597fdf31c9e35445776780ec2ccf2be477f14b5
Clement LG <clementlegruiec@gmail.com>
2025-03-16 10:26:15 +0100

    Video playback module added

--- web_app/app.py
+++ web_app/app.py
@@ -3,7 +3,8 @@ import os
 import random
 import logging
 from logging.handlers import RotatingFileHandler
-import time  # Importation du module time
+import time
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = "FakeSecretKey"  # Important pour les sessions et les flash messages
@@ -141,6 +142,14 @@ def payment():
 def process_payment():
     app.logger.info(f"Fake payment processed")
     return render_template('payment_result.html')
+
+@app.route('/video/<filename>')
+def show_video(filename):
+    return render_template('video.html', video_filename=filename)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
